cc2e8639d33222111e413f0f630e346c84c30961
Dev Enthusiastic <faraazhossainimran@gmail.com>
2024-01-23 23:53:34 +0600

    registration page add with navbar layout

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,16 +3,22 @@ import {
   } from "react-router-dom";
 import Main from "../layout/main";
 import Home from "../pages/Home/Home";
+import Registration from "../pages/Registration/Registration";
   
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
-      children: [ {
+      children: [ 
+        {
         path: "/", 
         element: <Home></Home>
-      }]
+      }, {
+        path: "/registration",
+        element: <Registration></Registration>
+      }
+    ]
     },
   ]);
 
